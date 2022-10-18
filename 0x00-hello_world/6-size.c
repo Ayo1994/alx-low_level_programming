8d15b813a2d11b424d74510c4449964ae777ef7d
Ayo1994 <olorunfemiay013@gmail.com>
2022-10-18 09:36:43 -0700

    Work

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,10 +3,9 @@
  * main - print out sizes of data types in c
  * code by ayo
  * Return: Always 0 (success)
- */
-
- int main(void)
- {	
+*/
+int main(void)
+{
 	 char a;
 	 int b;
 	 long int c;
@@ -19,5 +18,4 @@
 	 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 	 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	 return (0);
- }
-
+}
